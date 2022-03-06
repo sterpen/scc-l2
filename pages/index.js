6230f436e56bb7f1c5b3bcc9f9597fa4644fac9f
sterpen <82684942+sterpen@users.noreply.github.com>
2022-03-06 19:47:56 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ const Index = () => (
   <div className="mt-8 max-w-xl mx-auto px-8">
     <h1 className="text-center">
       <span className="block text-xl text-gray-600 leading-tight">
-        Welcome to
+        THE ONE AND ONLY mohamed bolbol 1001237 welcomes you to
       </span>
       <span className="block text-5xl font-bold leading-none">
         Cloud Computing Course
